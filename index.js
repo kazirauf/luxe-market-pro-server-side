a69b370b0116f53ed476310ca3d046e30e5bd5da
kazirauf <raufelahi9876@gmail.com>
2023-11-08 17:33:58 +0600

    res.send(result);

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
+
 
 
 
@@ -21,6 +30,25 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const logger = async (req, res, next) => {
+  console.log('called:', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+      return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+      }
+      req.user = decoded;
+      next();
+  })
+}
 
 async function run() {
   try {
@@ -40,6 +68,27 @@ run().catch(console.dir);
 const tabsCategoryCollections = client.db('luxeMarketProCollection').collection('CategoryNameCollection');
 const addJobsCollections = client.db('luxeMarketProCollection').collection('addJobsCollection');
 const JobsBidsCollection = client.db('luxeMarketProCollection').collection('JobsBidsCollection');
+ 
+
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  console.log('user for token', user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none'
+})
+    .send({ success: true });
+})
+
+app.post('/logout', async (req, res) => {
+  const user = req.body;
+  console.log('logging out', user);
+  res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+})
+
 // tabs
 app.get('/tabsCategory', async(req, res) => {
     const cursor = tabsCategoryCollections.find()
@@ -74,7 +123,7 @@ app.get("/allJobs", async(req, res) => {
   const result = await addJobsCollections.findOne(query);
   res.send(result);
 })
-//for the client my post page
+//for the client my post page require(crypto).randomBytes(64)
 app.put('/allJobs/:id', async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) }
@@ -95,8 +144,24 @@ app.put('/allJobs/:id', async (req, res) => {
   const result = await addJobsCollections.updateOne(filter, products, option);
   res.send(result)
 })
+app.put('/jobsBids/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }
+  const option = { upsert: true }
+  const updatedProducts = req.body;
+  const products = {
+      $set: {
+        status: updatedProducts.status,
+          
 
-app.get('/myPost',  async (req, res) => {
+      }
+  }
+  const result = await JobsBidsCollection.updateOne(filter, products, option);
+  res.send(result)
+})
+
+app.get('/myPost', verifyToken, logger,  async (req, res) => {
+  
   let query = {};
   if (req.query?.email) {
       query = { email: req.query.email }
@@ -118,7 +183,13 @@ app.post('/jobsBids', async(req, res) => {
   const result = await JobsBidsCollection.insertOne(newJobsBids)
   res.send(result)
 })
-app.get('/jobBids', async(req, res) => {
+app.get('/jobBids', logger, verifyToken, async(req, res) => {
+  console.log(req.query.email);
+  console.log('user in the valid token', req.user)
+  if(req.query.email !== req.user.email){
+      return res.status(403).send({message: 'forbidden access'})
+  }
+
   let query = {};
   if (req.query?.email) {
       query = { email: req.query.email }
@@ -127,7 +198,8 @@ app.get('/jobBids', async(req, res) => {
   res.send(result);
 })
 // for bids request page
-app.get('/bidRequest', async(req, res) => {
+app.get('/bidRequest', logger, verifyToken, async(req, res) => {
+
   let query = {};
   if (req.query?.buyer_email) {
       query = { buyer_email: req.query.buyer_email }
